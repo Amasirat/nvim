f089946bde9a0fe68e3bfe73c311f784ccd6bdb7
amir.basirat.2004@gmail.com <amir.basirat.2004@gmail.com>
2025-10-22 10:36:33 +0330

    added rust-analyzer

--- lua/main/remap.lua
+++ lua/main/remap.lua
@@ -1,2 +1,7 @@
 vim.g.mapleader = " "
+-- Come out to current directory
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- Save current working file 
+vim.keymap.set("n", "<leader>s", ":write<CR>", { silent = true })
+
+
